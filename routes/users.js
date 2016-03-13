33494781724341391155f3cda5ee3ef219c45160
Alexander Martin <martina52@mymacewan.ca>
2016-03-13 00:32:26 -0700

    Implement basic CRUD operations for users resource.  Passwords are NOT hashed.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var app = require('./../db_config.js');
+var users = require('./../modules/users.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/:id', function(req, res) {
+	users.retrieveSpecific(req.params.id, res);
+});
+
+router.get('/', function(req, res) {
+    // Parse query string.
+    var conditions = [];
+    for (key in req.query) {
+        var values = req.query[key].split(",");
+        conditions.push({
+            field: key,
+            values: values 
+        });
+    }
+    users.retrieve(conditions, res);
+});
+
+router.post('/', function(req, res) {
+    users.create(req.body, res);
+});
+
+router.put('/:id', function (req, res) {
+    users.update(req.params.id, req.body, res);
 });
 
 module.exports = router;
