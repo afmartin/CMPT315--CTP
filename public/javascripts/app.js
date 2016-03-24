9fca309e8c002c10c9246bbed7670cb2f00475ff
Jakob Bowering <boweringj@mymacewan.ca>
2016-03-23 20:35:05 -0600

    Add basic front page layout for client using angularjs

---
+++ public/javascripts/app.js
@@ -0,0 +1,74 @@
+
+(function() {
+    var app = angular.module('ctp', []);
+
+    app.controller("tabController", function(){
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+            this.tab = activeTab;
+        };
+    });
+
+
+    app.directive("mainTabs", function() {
+        return {
+            restrict: "E",
+            templateUrl: "../html/main-tabs.html",
+        };
+    });
+
+    app.directive("myDocs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "../html/my-docs.html"
+        };
+    });
+
+    app.directive("about", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "../html/about.html"
+        };
+    });
+
+    app.directive("home", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "../html/home.html"
+        };
+    });
+
+    app.directive("contact", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "../html/contact.html"
+        };
+    });
+
+    app.directive("footer", function() {
+        return {
+            restrict: 'A',
+            templateUrl: "../html/footer.html"
+        };
+    });
+
+    app.directive("header", function() {
+        return {
+            restrict: 'A',
+            templateUrl: "../html/header.html"
+        };
+    });
+
+    app.directive("sidebar", function(){
+       return{
+           restrict: 'E',
+           templateUrl: "../html/sidebar.html"
+       }
+    });
+
+})();
