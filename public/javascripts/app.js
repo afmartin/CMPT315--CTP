d275ce3953f75624c76303dd9184976eaef91ccf
Jakob Bowering <boweringj@mymacewan.ca>
2016-04-05 10:51:58 -0600

    Add login restriction to 'mydocs'

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@
 (function() {
     var app = angular.module('ctp', ["home", "users", "myDocs", "contact"]);
 
-    app.controller("mainController", function(){
+    app.controller("mainController",['$cookies', function($cookies){
         this.tab = 1;
 
         this.isSet = function(checkTab) {
@@ -10,9 +10,15 @@
         };
 
         this.setTab = function(activeTab) {
-            this.tab = activeTab;
+            if(activeTab == 2 && !$cookies.get('token')){
+                alert("You must be logged in to access this tab");
+                this.tab = 1;
+            }
+            else {
+                this.tab = activeTab;
+            }
         };
-    });
+    }]);
 
     app.directive("mainTabs", function() {
         return {
